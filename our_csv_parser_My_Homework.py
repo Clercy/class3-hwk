a925c6e3ded0fd6e4f9d25a406eed1e7a7396989
Luigi Clerici <luiclerici@yahoo.com>
2019-02-11 20:44:39 -0500

    Changes to typecasting in homework file

--- our_csv_parser_My_Homework.py
+++ our_csv_parser_My_Homework.py
@@ -1,57 +1,58 @@
 #!/usr/bin/env python
 
-import os
+def checkintfloat (s):
+
+    try:
+        return int (s)
+    except ValueError:
+        return  float (s)
+
 
 myfilename = "housing.data.txt"
 
-# if os.path.isfile(myfilename):
-#   print("yay, I have a file")
-#   if sky == blue:
-#     print('yay the sky is blue')
-# else:
-#   print ('boo, no files for me')
-
 with open(myfilename, 'r') as file_handle:
 
-#mylist = [] #new
-    
+    mylist = []
+
     for line in file_handle.readlines():
 
-        #mylist.clear() #new
-        
+        mylist.clear()
+
         line_clean = line.replace('   ', ' ').replace('  ', ' ')
         line_clean = line_clean.strip()
         values = line_clean.split(' ')
 
-        values = ('[%s]' %', '.join(map(str,values)))
+        for idx, item in enumerate(values):
 
-        #Tried to make this work but unsuccessful
-        #The For i loop worked in the interpreter
-        #but could not make it work here.
-        #replaced it with for idx but returns empty lists
+            y = values[idx]
+            y = y.replace("'","")
+            #print(type(checkintfloat(y)))
+            y = checkintfloat(y)
 
-        #for idx, item in enumerate(values):
-            #print(values[idx])
-            #For i in range(len(values)):     #issues with i
-            #x=values[idx]
-            #if type(values[idx]) == 'float':
-            #    x = float(values[idx])
+            #print(y)
 
-            #elif type(values[idx]) == 'str':
-            #    x = str(values[idx])
+            #print(type(y)) #shows all types for the list elements
+            mylist.insert(idx,y)
 
-            #elif type(values[idx]) == 'int':
-            #    x = int(values[idx])
-            #else:
-            #    x = values[idx]
-        #New
-        #mylist.insert(idx,x)
+        #for idx in range(len(values)):
+        #for i in range(len(values)):     #issues with i
+        	   #print(values[idx]      #x=values[idx]
+
         #print(mylist)
+        print('[%s]' % ', '.join(map(str, mylist)))
+
+
+
+        #[[mylist[jdx][idx] for jdx, row in enumerate(mylist) for idx, column in enumerate(mylist[0])
+
+        #print(mylist)
+        #print(values)
 
 
 
 
-        print(values)
+
+
         #for value in values:
             # for homework:
             # identify what type of data each value is, and cast it
@@ -60,4 +61,5 @@ with open(myfilename, 'r') as file_handle:
             # I.e. ['0.04741', '0.00', '11.930', '0', '0.5730', '6.0300', '80.80', '2.5050', '1', '273.0', '21.00', '396.90', '7.88', '11.90']
             # becomes: [0.04741, 0.0, 11.93, 0, 0.573, '6.03, 80.8, 2.505, 1, 273.0, 21.0, 396.90, 7.88, 11.90]
             #print(float(value))
-    print('finished!')
+
+		#print('finished!')
